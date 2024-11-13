0cac799e5b4df448a2b2231fb3eaaa504feedffb
jeremu2907 <jeremu2907@gmail.com>
2024-11-13 03:06:20 -0600

    first commit

---
+++ src/test/java/jeremu2907/cocktail/CocktailApplicationTests.java
@@ -0,0 +1,13 @@
+package jeremu2907.cocktail;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CocktailApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
