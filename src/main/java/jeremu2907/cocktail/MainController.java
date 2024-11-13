6fc64f79cd64e33a1fd861c1a14815e14a4692f2
jeremu2907 <jeremu2907@gmail.com>
2024-11-13 13:29:19 -0600

    delete method

--- src/main/java/jeremu2907/cocktail/MainController.java
+++ src/main/java/jeremu2907/cocktail/MainController.java
@@ -1,11 +1,14 @@
 package jeremu2907.cocktail;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -27,11 +30,6 @@ public class MainController {
         return "success";
     }
 
-    @GetMapping(path="/all")
-    public @ResponseBody Iterable<Recipe> getAllRecipe() {
-        return recipeRepository.findAll();
-    }
-
     @PatchMapping(path="/patch")
     public @ResponseBody String editRecipe(
         @RequestBody Recipe recipe
@@ -42,5 +40,27 @@ public class MainController {
         r.setContent(recipe.getContent());
         recipeRepository.save(r);
         return "success";
+    }
+
+    @DeleteMapping(path="/delete")
+    public @ResponseBody ResponseEntity<String> deleteRecipe(
+        @RequestParam("id") Integer id
+    )
+    {
+        recipeRepository.deleteById(id);
+        return ResponseEntity.noContent().build();
+    }
+
+    @GetMapping(path="/all")
+    public @ResponseBody Iterable<Recipe> getAllRecipe() {
+        return recipeRepository.findAll();
+    }
+
+    @GetMapping(path="/get")
+    public @ResponseBody Recipe get(
+        @RequestParam("id") Integer id
+    )
+    {
+        return recipeRepository.findById(id).get();
     }
 }
