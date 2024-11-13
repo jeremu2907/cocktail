6d635df592556132223de32a543e955dc76b10d2
jeremu2907 <jeremu2907@gmail.com>
2024-11-13 04:03:21 -0600

    removed unused imports

--- src/main/java/jeremu2907/cocktail/MainController.java
+++ src/main/java/jeremu2907/cocktail/MainController.java
@@ -1,12 +1,10 @@
 package jeremu2907.cocktail;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.security.SecurityProperties.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
 
--- src/main/java/jeremu2907/cocktail/RecipeRepository.java
+++ src/main/java/jeremu2907/cocktail/RecipeRepository.java
@@ -2,8 +2,6 @@ package jeremu2907.cocktail;
 
 import org.springframework.data.repository.CrudRepository;
 
-import jeremu2907.cocktail.Recipe;
-
 public interface RecipeRepository extends CrudRepository<Recipe, Integer> {
     
 }
